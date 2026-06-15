1c5a957196929187806b3de4faa3a6d8cf3c58b7
ilyakkk75 <89085091885iliya@mail.ru>
2026-06-15 16:45:24 +0300

    dz

---
+++ dz 10.06.py
@@ -0,0 +1,30 @@
+#z1
+# a=int(input())
+# b=int(input())
+# c=0
+# for i in range(a, b+1):
+#     c+=i
+# print(c)
+# z2
+# a=int(input())
+# b=int(input())
+# c=min(a,b)
+# while a%c!=0 or b%c!=0:
+#     c-=1
+# print(c)
+# z3
+# a=int(input())
+# c=1
+# while c<=a:
+#     if a%c==0:
+#         print(c)
+#     c+=1
+# z4
+# a=int(input())
+# c=0
+# while a>0:
+#     a=a//10
+#     c+=1
+# print(c)
+# z5
+# idk
