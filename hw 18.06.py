dc7f4f753559f047d202060030dee6acf276de8c
ilyakkk75 <89085091885iliya@mail.ru>
2026-06-21 19:10:44 +0300

    dz

---
+++ hw 18.06.py
@@ -0,0 +1,45 @@
+# NUMГ
+# n=int(input())
+# for i in range (n):
+#     print((' ' *(n-i-1))+'* '*(i+1))
+
+# NUMБ
+# a=int(input())
+# for i in range (a):
+#     print('*'*(i+1))
+#
+# a = int(input())
+# for i in range(a,0,-1):
+#     print('* '*(i))
+
+
+# numk
+# a=int(input())
+# for i in range (1,a+1):
+#     print('  '*(a-i)+'* '*(i))
+
+# numA
+# a=int(input())
+# for i in range(a):
+#     for j in range(a):
+#         if i<=j:
+#             print('*', end=' ')
+#         else:
+#             print(' ',end=' ')
+#     print()
+
+
+
+
+
+
+# # NUMB
+# a=int(input())
+# for i in range((a+1)//2):
+#     print(' '*(i)+'*'*(a)+' '*i)
+#     a=a-2
+
+# # NUMИ
+# a = int(input())
+# for i in range(a,0,-1):
+#     print((' '+'*')*(i))
