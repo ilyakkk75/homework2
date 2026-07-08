bbfdf896a0d8e89dc5b1eb812dc3d31793dfb081
ilyakkk75 <89085091885iliya@mail.ru>
2026-07-09 00:29:51 +0300

    dz

---
+++ dz to 9,07.26.py
@@ -0,0 +1,84 @@
+a=[[2,3,4,21],
+   [23,5,4,3],
+   [4,5,2,45]]
+# for i in range (len(a)):
+#     sum=0
+#     max=a[i][0]
+#     min=a[i][0]
+#     max_i=0
+#     min_i=0
+#     for j in range(len(a[i])):
+#         sum+=a[i][j]
+#         if min>a[i][j]:
+#             min=a[i][j]
+#             min_i=j
+#         if max<a[i][j]:
+#             max=a[i][j]
+#             max_i=j
+#     print(i)
+#     print(sum/len(a[i]))
+#     print(sum)
+#     print(max)
+#     print(max_i)
+#     print(min)
+#     print(min_i)
+#     print()
+
+
+
+
+
+
+# for j in range(len(a[0])):
+#     sum=0
+#     max=a[0][j]
+#     min=a[0][j]
+#     max_i=0
+#     min_i=0
+#     for i in range(len(a)):
+#         sum+=a[i][j]
+#         if max<a[i][j]:
+#             max=a[i][j]
+#             max=j
+#         if min>a[i][j]:
+#             min=a[i][j]
+#             min_i=j
+#     print(j)
+#     print(sum/len(a))
+#     print(sum)
+#     print(max)
+#     print(max_i)
+#     print(min)
+#     print(min_i)
+#     print()
+
+
+
+# max=a[0][0]
+# min=a[0][0]
+# max_i=0
+# min_i=0
+# sum=0
+# delit=0
+# for i in range(len(a)):
+#     for j in range(len(a[i])):
+#         sum+=a[i][j]
+#         delit+=1
+#         if max<a[i][j]:
+#             max=a[i][j]
+#             max_i=j
+#         if min>a[i][j]:
+#             min=a[i][j]
+#             min_i=j
+# print(sum)
+# print(sum/delit)
+# print(min_i)
+# print(min)
+# print(max_i)
+# print(max)
+
+
+
+
+
+
