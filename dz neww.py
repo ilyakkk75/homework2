533b07cc84c21b6a0ed835b994c3bdafd2285372
ilyakkk75 <89085091885iliya@mail.ru>
2026-07-16 16:50:00 +0300

    dz

---
+++ dz neww.py
@@ -0,0 +1,28 @@
+# def words(a):
+#     lt=a.split( )
+#     for i in range(len(lt)):
+#         i+=1
+#     print(i)
+
+
+# def num(a):
+#     numb=0
+#     lt=list(map(int,a.split( )))
+#     for i in lt:
+#         if i>=10:
+#             numb+=1
+#     print(numb)
+# a='10 1 12 2 12'
+# num(a)
+
+
+# def wor(a):
+#     lt=a.split( )
+#     result=''
+#     for i in lt:
+#         if lt.count(i)>=1:
+#             if i not in result:
+#                 result+=i
+#                 print(result)
+#     return result
+# вроде даже ии сказал,что правильно,но,почему-то,выводит криво.пожалуйста,объясните,что не так?
